fc86ca027cd17c36183810ee6edd4982b01f26b7
Maksym Wesolowski <weso.pl@gmail.com>
2025-04-21 11:26:55 +0200

    functions generate_views and top_titles added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Production:
     def __init__(self, title, year, genre):
         self.title = title
@@ -32,6 +34,7 @@ def get_movies(library):
             print(i)
         else:
             continue
+    return library
 
 def get_series(library):
     library = sorted(library, key=lambda series: series.title.lower())
@@ -40,6 +43,7 @@ def get_series(library):
             print(i)
         else:
             continue
+    return library
 
 def search(title, library):
     for i in library:
@@ -49,22 +53,49 @@ def search(title, library):
     print(f"brak filmu/serialu {title} w bibliotece")
     return False
 
+def generate_views(library):
+    random_production = random.choice(library)
+    play = random.randint(1,100)
+    random_production.played =+ play
+
+def generate_views_x10(library):
+    for i in range(10):
+        generate_views(library)
+
+def top_titles(library, content_type):
+    library = sorted(library, key=lambda production: production.played, reverse=True)
+
+    if content_type == "filmy":
+        top = [i for i in library if isinstance(i, Movie)]
+        print("Najpopularniejsze filmy:")
+        for movie in top[:3]:
+            print(f"{movie.title} Ilość odtworzeń {movie.played}" )
+        return top
+    elif content_type == "seriale":
+        top = [i for i in library if isinstance(i, Series)]
+        print("Najpopularniejsze seriale:")
+        for series in top[:3]:
+            print(f"{series.title} Ilość odtworzeń {series.played}")
+
+
 library = []
 
 movie1 = Movie(title="Pulp Fiction", year=1994, genre="Crime")
 movie2 = Movie(title="Django", year=2012, genre="western")
+movie3 = Movie(title="Gwiezdne wojny nowa nadzieja",year=1977, genre="Science fiction")
+movie4 = Movie(title="Chłopaki nie płaczą", year=2000, genre="comedy")
 series1 = Series(title="Californication", year=2007, genre="drama", episode=1, season=1)
 series2 = Series(title="Dexter", year=2006, genre="drama", episode=2, season=3)
 library.append(movie1)
 library.append(series1)
 library.append(movie2)
 library.append(series2)
+library.append(movie3)
+library.append(movie4)
 
-movie1.play()
-movie2.play()
-movie2.play()
-series1.play()
-
+generate_views_x10(library)
 get_movies(library)
 get_series(library)
-print(search("dexter", library))
+search("deaxter", library)
+
+top_titles(library, "filmy")
